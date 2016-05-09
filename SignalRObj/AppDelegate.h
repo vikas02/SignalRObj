919c9b05bfd07274e64fd700fcbaa824ef418230
Vikas <Vikas@vikas.chetu.com>
2016-05-09 17:59:37 +0530

    Initial Commit

---
+++ SignalRObj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SignalRObj
+//
+//  Created by Vikas on 09/05/16.
+//  Copyright © 2016 FlightTrak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SignalRObj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SignalRObj
+//
+//  Created by Vikas on 09/05/16.
+//  Copyright © 2016 FlightTrak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
